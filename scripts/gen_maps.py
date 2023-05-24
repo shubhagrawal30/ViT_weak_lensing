1abc9e06f28e587be669054914461c5ae92046ae
Shubh Agrawal <shubhagrawal30@gmail.com>
2023-05-24 09:22:32 -0700

    add data to json for my maps

--- scripts/gen_maps.py
+++ scripts/gen_maps.py
@@ -27,39 +27,46 @@ def IndexToDeclRa(index, nside,nest= False):
 
 def gen_from_one_file(fil_name, nside=512, nside_crop=4, \
                       keys=["stage3_lensing" + str(i) for i in range(1, 5)], out_shape=(224, 224, 4)):
-    kappas = {}
-    with h5py.File(fil_name) as fil:
-        for key in fil['kg'].keys():
-            kappas[key] = np.array(fil['kg'][key])
-    
-    map_indexes = np.arange(hp.nside2npix(nside_crop))
-    dec_,ra_ = IndexToDeclRa(map_indexes,nside_crop,nest=False)
-    pairs_ = np.vstack([ra_,dec_])
-    res = hp.nside2resol(nside, arcmin=True)
-    xsize=out_shape[0]
-    
-    pairs_ = pairs_[:, np.random.choice(np.arange(pairs_.shape[1]), 10, replace=False)]
-    
-    out_arr = np.empty((pairs_.shape[1], *out_shape))
-    
-    for chan, key in enumerate(keys):
-        kappa = np.array(kappas[key])
+    try:
+        kappas = {}
+        with h5py.File(fil_name) as fil:
+            for key in fil['kg'].keys():
+                kappas[key] = np.array(fil['kg'][key])
 
-        for i in range(pairs_.shape[1]):
-            m_projected = hp.gnomview(kappa, rot=pairs_[:,i], xsize=xsize,\
-                                      no_plot=True,reso=res,return_projected_map=True)
-            out_arr[i, :, :, chan] = m_projected
-    return out_arr
+        map_indexes = np.arange(hp.nside2npix(nside_crop))
+        dec_,ra_ = IndexToDeclRa(map_indexes,nside_crop,nest=False)
+        pairs_ = np.vstack([ra_,dec_])
+        res = hp.nside2resol(nside, arcmin=True)
+        xsize=out_shape[0]
+
+        pairs_ = pairs_[:, np.random.choice(np.arange(pairs_.shape[1]), 10, replace=False)]
+
+        out_arr = np.empty((pairs_.shape[1], *out_shape))
+
+        for chan, key in enumerate(keys):
+            kappa = np.array(kappas[key])
+
+            for i in range(pairs_.shape[1]):
+                m_projected = hp.gnomview(kappa, rot=pairs_[:,i], xsize=xsize,\
+                                          no_plot=True,reso=res,return_projected_map=True)
+                out_arr[i, :, :, chan] = m_projected
+        return out_arr
+    except:
+        print("failed", fil_name)
 
 def for_one_grid(grid):
     try:
+    # if 1:
         path_grid = os.path.join(grids_dir, grid)
         all_perms_out = np.ndarray((0, 224, 224, 4))
         for perm in os.listdir(path_grid):
             if "perm" not in perm:
                 continue
             out_arr = gen_from_one_file(os.path.join(path_grid, perm, fil_name))
-            all_perms_out = np.append(all_perms_out, out_arr, axis=0)
+            if out_arr is None:
+                print("skipping", perm)
+            else:
+                all_perms_out = np.append(all_perms_out, out_arr, axis=0)
         np.save(os.path.join(out_dir, grid), all_perms_out)
     except:
         print("failed", grid)
