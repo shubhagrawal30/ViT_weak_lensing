a98adf56fec73c16f58b4fbe759ca6ef5cfaa7ad
Shubh <shubh@sas.upenn.edu>
2023-06-26 15:47:30 -0400

    add vit with nll

--- ViT.py
+++ ViT.py
@@ -1,177 +1,167 @@
-from datasets import load_dataset
-from pathlib import Path
-from transformers import AutoImageProcessor, ViTForImageClassification, ViTConfig, ViTModel
-import numpy as np 
-from matplotlib import pyplot as plt
-from PIL import Image
-import torch
-from transformers import create_optimizer, TrainingArguments, Trainer
-from torch.utils.data import DataLoader
+if __name__ == "__main__":
+    from datasets import load_dataset
+    from pathlib import Path
+    from transformers import AutoImageProcessor, ViTForImageClassification, ViTConfig, ViTModel
+    import numpy as np 
+    from matplotlib import pyplot as plt
+    from PIL import Image
+    import torch
+    from transformers import create_optimizer, TrainingArguments, Trainer
+    from torch.utils.data import DataLoader
 
-out_name = "20230620_vit"
-out_dir = f"./models/{out_name}/"
-plot_dir = f"./plots/{out_name}/"
-Path(out_dir).mkdir(parents=True, exist_ok=True)
+    import sys
+    sys.path.append("./scripts/")
+    from TrainerWithDropout import DropoutTrainer
+    from ViTwithNLL import NLLViT
 
-subset = "train"
-labels = ["H0", "Ob", "Om", "ns", "s8", "w0"]
-size = (224, 224)
-batch_size = 32
-num_epochs = 2
-learning_rate = 0.001
-weight_decay_rate = 0.001
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    print(device)
 
-data = load_dataset("./data/20230419_224x224/20230419_224x224.py", cache_dir="~/cache")
+    out_name = "20230620_vit"
+    out_dir = f"./models/{out_name}/"
+    plot_dir = f"./plots/{out_name}/"
+    Path(out_dir).mkdir(parents=True, exist_ok=True)
 
-id2label = {labels.index(label):label for label in data["train"].features if label in labels}
-label2id = {label:id for id,label in id2label.items()}
+    subset = "test"
+    # labels = ["H0", "Ob", "Om", "ns", "s8", "w0"]
+    labels = ["Om", "s8"]
+    size = (224, 224)
+    batch_size = 32
+    num_epochs = 2
+    learning_rate = 0.001
+    weight_decay_rate = 0.001
+    
+    id2label = {**{labels.index(label):label for label in labels}}#, \
+                # **{(len(labels)+labels.index(label)): "ln_sig_" + label for label in labels}}
+    label2id = {label:id for id,label in id2label.items()}
+
+    print(id2label)
+    print(label2id)
+
+    data = load_dataset("./data/20230419_224x224/20230419_224x224.py", cache_dir="~/cache")
+
+    checkpoint = "google/vit-base-patch16-224-in21k"
+    model = NLLViT.from_pretrained(checkpoint,
+                problem_type = "regression", id2label=id2label, label2id=label2id, hidden_dropout_prob=0.1,
+                num_channels=4, image_size=224, patch_size=16, ignore_mismatched_sizes=True)
+
+    print(model.config.problem_type)
+    print(model.config.num_labels)
+
+    from torchvision.transforms import (CenterCrop, 
+                                        Compose, 
+                                        Normalize, 
+                                        RandomHorizontalFlip,
+                                        RandomVerticalFlip,
+                                        RandomResizedCrop, 
+                                        Resize, 
+                                        ToTensor)
+
+    normalize = lambda img: (img - torch.mean(img)) / torch.std(img)
+
+    train_data_augmentation = Compose(
+            [
+                RandomHorizontalFlip(),
+                RandomVerticalFlip(),
+                normalize,
+            ]
+        )
+
+    val_data_augmentation = Compose(
+            [
+                normalize,
+            ]
+        )
+
+    def preprocess_train(examples):
+        # examples["labels"] = np.transpose([examples[x] for x in examples.keys() if x != "map"]).astype(np.float32)
+        examples["labels"] = np.transpose([examples[x] for x in labels]).astype(np.float32)
+        examples['pixel_values'] = [train_data_augmentation(torch.swapaxes(torch.Tensor(np.array(image)), 0, 2)) for image in examples['map']]
+        return examples
+
+    def preprocess_val(examples):
+        # examples["labels"] = np.transpose([examples[x] for x in examples.keys() if x != "map"]).astype(np.float32)
+        examples["labels"] = np.transpose([examples[x] for x in labels]).astype(np.float32)
+        examples['pixel_values'] = [val_data_augmentation(torch.swapaxes(torch.Tensor(np.array(image)), 0, 2)) for image in examples['map']]
+        return examples
+
+    data["train"].set_transform(preprocess_train)
+    data["validation"].set_transform(preprocess_val)
+    data["test"].set_transform(preprocess_val)
 
 
-checkpoint = "google/vit-base-patch16-224-in21k"
-model = ViTForImageClassification.from_pretrained(checkpoint,
-            problem_type = "regression", id2label=id2label, label2id=label2id, hidden_dropout_prob=0.1,
-            num_channels=4, image_size=224, patch_size=16, ignore_mismatched_sizes=True)
+    def collate_fn(examples):
+        pixel_values = torch.stack([example["pixel_values"] for example in examples])
+        labels = torch.tensor([example["labels"] for example in examples])
+        return {"pixel_values": pixel_values, "labels": labels}
 
-print(model.config.problem_type)
-print(model.config.num_labels)
-
-
-
-from torchvision.transforms import (CenterCrop, 
-                                    Compose, 
-                                    Normalize, 
-                                    RandomHorizontalFlip,
-                                    RandomVerticalFlip,
-                                    RandomResizedCrop, 
-                                    Resize, 
-                                    ToTensor)
-
-normalize = lambda img: (img - torch.mean(img)) / torch.std(img)
-
-train_data_augmentation = Compose(
-        [
-            RandomHorizontalFlip(),
-            RandomVerticalFlip(),
-            normalize,
-        ]
+    args = TrainingArguments(
+        f"./temp/dropout",
+        save_strategy="epoch",
+        evaluation_strategy="epoch",
+        num_train_epochs=num_epochs,
+        weight_decay=weight_decay_rate,
+        load_best_model_at_end=True,
+        logging_dir='logs',
+        remove_unused_columns=False,
+        per_device_train_batch_size = batch_size,
+        per_device_eval_batch_size = batch_size
     )
 
-val_data_augmentation = Compose(
-        [
-            normalize,
-        ]
+    trainer = DropoutTrainer(
+        model,
+        args,
+        train_dataset= data[subset],
+        eval_dataset= data["validation"],
+        data_collator=collate_fn
     )
 
-def preprocess_train(examples):
-    # examples["labels"] = np.transpose([examples[x] for x in examples.keys() if x != "map"]).astype(np.float32)
-    examples["labels"] = np.transpose([examples[x] for x in labels]).astype(np.float32)
-    examples['pixel_values'] = [train_data_augmentation(torch.swapaxes(torch.Tensor(np.array(image)), 0, 2)) for image in examples['map']]
-    return examples
+    trainer.train()
 
-def preprocess_val(examples):
-    # examples["labels"] = np.transpose([examples[x] for x in examples.keys() if x != "map"]).astype(np.float32)
-    examples["labels"] = np.transpose([examples[x] for x in labels]).astype(np.float32)
-    examples['pixel_values'] = [val_data_augmentation(torch.swapaxes(torch.Tensor(np.array(image)), 0, 2)) for image in examples['map']]
-    return examples
+    trainer.save_model(out_dir)
 
-data["train"].set_transform(preprocess_train)
-data["validation"].set_transform(preprocess_val)
-data["test"].set_transform(preprocess_val)
+    n_pred = 10
+    preds = np.empty((n_pred, len(data["validation"]), 6))
+    for i in range(n_pred):
+        if i % 2 == 0: 
+            print(i)
+        ps = trainer.predict(data["validation"])
+        preds[i] = ps.predictions
+        if i == 0:
+            label_ids = ps.label_ids
 
+    plot_y = label_ids
+    predictions_best = np.nanmean(preds, axis=0)
+    predictions_std = np.nanstd(preds, axis=0)
 
-def collate_fn(examples):
-    pixel_values = torch.stack([example["pixel_values"] for example in examples])
-    labels = torch.tensor([example["labels"] for example in examples])
-    return {"pixel_values": pixel_values, "labels": labels}
+    upp_lims = np.nanmax(plot_y, axis=0)
+    low_lims = np.nanmin(plot_y, axis=0)
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(10, 5))
+    fig.subplots_adjust(wspace=0.3, hspace=0.2)
+    labels = [r"$\Omega_m$", r"$\sigma_8$"]
+    for ind, (label, ax, low_lim, upp_lim) in enumerate(zip(labels, axs.ravel(), low_lims, upp_lims)):
+        p = np.poly1d(np.polyfit(plot_y[:, ind], predictions_best[:, ind], 1))
+        # ax.errorbar(plot_y[:, ind][::10], predictions_best[:, ind][::10],  predictions_std[:, ind][::10], marker="x", ls='none', alpha=0.4)
+        ax.errorbar(plot_y[:, ind][::10], predictions_best[:, ind][::10],  0, marker="x", ls='none', alpha=0.4)
+        ax.set_xlabel("true")
+        ax.set_ylabel("prediction")
+        ax.plot([low_lim, upp_lim], [low_lim, upp_lim], color="black")
+        ax.plot([low_lim, upp_lim], [p(low_lim), p(upp_lim)], color="black", ls=":")
+        ax.set_xlim([low_lim, upp_lim])
+        ax.set_ylim([low_lim, upp_lim])
+        ax.set_aspect('equal', adjustable='box')
+        ax.set_title(label)
+        ax.grid()
+    plt.savefig(plot_dir + "pred-true.png")
+    plt.close()
 
-train_dataloader = DataLoader(data[subset], collate_fn=collate_fn, batch_size=4)
-
-batch = next(iter(train_dataloader))
-for k,v in batch.items():
-    if isinstance(v, torch.Tensor):
-        print(k, v.shape)
-        
-num_train_steps = len(data[subset]) * num_epochs
-optimizer, lr_schedule = create_optimizer(
-    init_lr=learning_rate,
-    num_train_steps=num_train_steps,
-    weight_decay_rate=weight_decay_rate,
-    num_warmup_steps=0,
-)
-
-args = TrainingArguments(
-    f"./temp/dropout",
-    save_strategy="epoch",
-    evaluation_strategy="epoch",
-    num_train_epochs=num_epochs,
-    weight_decay=weight_decay_rate,
-    load_best_model_at_end=True,
-    logging_dir='logs',
-    remove_unused_columns=False,
-)
-
-import sys
-sys.path.append("./scripts/")
-from TrainerWithDropout import DropoutTrainer
-
-import torch
-
-trainer = DropoutTrainer(
-    model,
-    args,
-    train_dataset= data[subset],
-    eval_dataset= data["validation"],
-    data_collator=collate_fn
-)
-
-
-trainer.train()
-
-
-trainer.save_model(out_dir)
-
-n_pred = 10
-preds = np.empty((n_pred, len(data["validation"]), 6))
-for i in range(n_pred):
-    if i % 2 == 0: 
-        print(i)
-    ps = trainer.predict(data["validation"])
-    preds[i] = ps.predictions
-    if i == 0:
-        label_ids = ps.label_ids
-
-plot_y = label_ids
-predictions_best = np.nanmean(preds, axis=0)
-predictions_std = np.nanstd(preds, axis=0)
-
-upp_lims = np.nanmax(plot_y, axis=0)
-low_lims = np.nanmin(plot_y, axis=0)
-fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(10, 5))
-fig.subplots_adjust(wspace=0.3, hspace=0.2)
-labels = [r"$\Omega_m$", r"$\sigma_8$"]
-for ind, (label, ax, low_lim, upp_lim) in enumerate(zip(labels, axs.ravel(), low_lims, upp_lims)):
-    p = np.poly1d(np.polyfit(plot_y[:, ind], predictions_best[:, ind], 1))
-    # ax.errorbar(plot_y[:, ind][::10], predictions_best[:, ind][::10],  predictions_std[:, ind][::10], marker="x", ls='none', alpha=0.4)
-    ax.errorbar(plot_y[:, ind][::10], predictions_best[:, ind][::10],  0, marker="x", ls='none', alpha=0.4)
-    ax.set_xlabel("true")
-    ax.set_ylabel("prediction")
-    ax.plot([low_lim, upp_lim], [low_lim, upp_lim], color="black")
-    ax.plot([low_lim, upp_lim], [p(low_lim), p(upp_lim)], color="black", ls=":")
-    ax.set_xlim([low_lim, upp_lim])
-    ax.set_ylim([low_lim, upp_lim])
-    ax.set_aspect('equal', adjustable='box')
-    ax.set_title(label)
-    ax.grid()
-plt.savefig(plot_dir + "pred-true.png")
-plt.close()
-
-fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
-fig.subplots_adjust(wspace=0.3, hspace=0.2)
-labels = [r"$\Omega_m$", r"$\sigma_8$"]
-for ind, (label, ax) in enumerate(zip(labels, axs.ravel())):
-    ax.hist((plot_y[:, ind] - predictions_best[:, ind]) / predictions_best[:, ind], bins=100, density=True, histtype="step")
-    ax.set_title(label)
-    ax.grid()
-    ax.set_xlim([-0.7, 0.7])
-plt.savefig(plot_dir + "hist.png")
-plt.close()
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+    fig.subplots_adjust(wspace=0.3, hspace=0.2)
+    labels = [r"$\Omega_m$", r"$\sigma_8$"]
+    for ind, (label, ax) in enumerate(zip(labels, axs.ravel())):
+        ax.hist((plot_y[:, ind] - predictions_best[:, ind]) / predictions_best[:, ind], bins=100, density=True, histtype="step")
+        ax.set_title(label)
+        ax.grid()
+        ax.set_xlim([-0.7, 0.7])
+    plt.savefig(plot_dir + "hist.png")
+    plt.close()
---
+++ scripts/ViTwithNLL.py
@@ -0,0 +1,89 @@
+from transformers import AutoImageProcessor, ViTForImageClassification, ViTConfig, ViTModel
+
+from typing import Dict, List, Optional, Set, Tuple, Union
+
+import torch
+import torch.utils.checkpoint
+from torch import nn
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+
+from transformers.modeling_outputs import (
+    BaseModelOutput,
+    BaseModelOutputWithPooling,
+    ImageClassifierOutput,
+    MaskedImageModelingOutput,
+)
+
+def neg_log_likelihood(preds, y):
+    # assuming first half of predictions are means and second half are log variances
+    means, log_vars = preds[:, :preds.shape[1]//2], preds[:, preds.shape[1]//2:]
+    error = y - means
+    return torch.mean(0.5 * torch.exp(-log_vars) * error * error + 0.5 * log_vars)
+
+class NLLViT(ViTForImageClassification):
+    def forward(
+        self,
+        pixel_values: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        interpolate_pos_encoding: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[tuple, ImageClassifierOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the image classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.vit(
+            pixel_values,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            interpolate_pos_encoding=interpolate_pos_encoding,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+
+        logits = self.classifier(sequence_output[:, 0, :])
+
+        loss = None
+        if labels is not None:
+            # move labels to correct device to enable model parallelism
+            labels = labels.to(logits.device)
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = neg_log_likelihood
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return ImageClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
